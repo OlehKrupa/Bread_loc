39d5b287447824b76b469ed5cb7790202b41ef8b
Oleh <95093144+OlehKrupa@users.noreply.github.com>
2023-01-30 23:03:46 +0200

    Update footer.php

--- templates/partials/footer.php
+++ templates/partials/footer.php
@@ -1,13 +1,3 @@
-<footer class="text-center pt-4 my-md-5 pt-md-2 border-top">
-	<div class="row">
-		<div class="col col-md">
-			<h5>Диплом</h5>
-			<ul class="list-unstyled text_small">
-				<li><a href="" class="link-secondary">Розробник</a></li>
-			</ul>
-		</div> 
 
-	</div>
-</footer>
 </body>
 </html>
