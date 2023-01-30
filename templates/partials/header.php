aedc9d7bbf9e11b657680fd1ffb9e02b521e4d0e
Oleh <95093144+OlehKrupa@users.noreply.github.com>
2023-01-30 23:03:48 +0200

    Update header.php

--- templates/partials/header.php
+++ templates/partials/header.php
@@ -2,7 +2,6 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title>Forms</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
@@ -10,6 +9,33 @@
     crossorigin="anonymous"></script>
 </head>
 <body>
-    <div class="container text-center">
-        header
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark" aria-label="Eighth navbar example">
+    <div class="container">
+      <a class="navbar-brand" href="#">ЕлеваторЮА</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarsExample07">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Дім</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Посилання</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Вимкнено</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="dropdown07" data-bs-toggle="dropdown" aria-expanded="false">Список</a>
+            <ul class="dropdown-menu" aria-labelledby="dropdown07">
+              <li><a class="dropdown-item" href="#">Дія 1</a></li>
+              <li><a class="dropdown-item" href="#">Дія 2</a></li>
+              <li><a class="dropdown-item" href="#">Дія 3</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
     </div>
+  </nav>
