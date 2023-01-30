cea68afee54366465673a1125f1ac015d7b0e8ff
Oleh <95093144+OlehKrupa@users.noreply.github.com>
2023-01-30 23:03:43 +0200

    DB Connection

--- config.php
+++ config.php
@@ -1,7 +1,13 @@
 <?php
 session_start();
-
+ 
 define("ROOT_PATH", dirname(__FILE__));
 define("TEMPLATES_PATH", dirname(__FILE__).DIRECTORY_SEPARATOR."templates".DIRECTORY_SEPARATOR);
+
+define("DB_NAME","bread");
+define("DB_USER","grain_administrator");
+define("DB_USER_PASS","Crop1234@");
+
+$dbConnect = new PDO('mysql:host=localhost;dbname='.DB_NAME,DB_USER,DB_USER_PASS);
 
 ?>
