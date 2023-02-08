df5f858cebb7dfe061ca2e93e1898b333d5da369
Oleh <95093144+OlehKrupa@users.noreply.github.com>
2023-02-08 23:59:00 +0200

    Update crop.php

--- templates/crop.php
+++ templates/crop.php
@@ -2,27 +2,74 @@
 
 <div class="container-lg">
 
-<div class="container-lg text-center row g-0">
+	<form action="crop.php" method="post">
+		
+		<div class="container-lg text-center row g-0">
+			<div class="col mx-2">
+				<div class="input-group m-2">
+					<span class="input-group-text" id="basic-addon1">Постачальник</span>
+					<select class="form-select" id="supplier_select">
+						<?php foreach($supplier as $key=>$value): ?>
+							<option value="<?php echo $value["id"];?>"><?php echo $value["name"];?></option>
+						<?php endforeach; ?>
+					</select>
+				</div>
+				<div class="input-group m-2">
+					<span class="input-group-text" id="basic-addon1">Склад</span>
+					<select class="form-select" id="warehouse_select">
+						<?php foreach($warehouse as $key=>$value): ?>
+							<option value="<?php echo $value["id"];?>"><?php echo $value["name"];?></option>
+						<?php endforeach; ?>
+					</select>
+				</div>
+				<div class="input-group m-2">
+					<span class="input-group-text" id="basic-addon1">Кількість тон</span>
+					<input type="text" class="form-control" placeholder="" aria-label="Username" aria-describedby="basic-addon1">
+				</div>
+				<div class="input-group m-2">
+					<span class="input-group-text" id="basic-addon1">Культура</span>
+					<input type="text" class="form-control" placeholder="" aria-label="Username" aria-describedby="basic-addon1">
+				</div>
+				<div class="input-group m-2">
+					<span class="input-group-text" id="basic-addon1">Сорт</span>
+					<input type="text" class="form-control" placeholder="" aria-label="Username" aria-describedby="basic-addon1">
+				</div>
+			</div>
+			<div class="col mx-2">
+				<div class="input-group m-2">
+					<span class="input-group-text" id="basic-addon1">Стандарт</span>
+					<select class="form-select" id="standard_select">
+						<?php foreach($standart as $key=>$value): ?>
+							<option value="<?php echo $value["id"];?>"><?php echo $value["name"];?></option>
+						<?php endforeach; ?>
+					</select>
+				</div>
+				<div class="input-group m-2">
+					<span class="input-group-text" id="basic-addon1">Вологість</span>
+					<input type="text" class="form-control" placeholder="" aria-label="Username" aria-describedby="basic-addon1">
+				</div>
+				<div class="input-group m-2">
+					<span class="input-group-text" id="basic-addon1">Сміття</span>
+					<input type="text" class="form-control" placeholder="" aria-label="Username" aria-describedby="basic-addon1">
+				</div>
+				<div class="input-group m-2">
+					<span class="input-group-text" id="basic-addon1">PO₄</span>
+					<input type="text" class="form-control" placeholder="" aria-label="Username" aria-describedby="basic-addon1">
+				</div>
+				<div class="input-group m-2">
+					<span class="input-group-text" id="basic-addon1">г/л</span>
+					<input type="text" class="form-control" placeholder="" aria-label="Username" aria-describedby="basic-addon1">
+				</div>
+			</div>
+		</div>
 
-		<div class="input-group m-2">
-  <span class="input-group-text" id="basic-addon1">@</span>
-  <input type="text" class="form-control" placeholder="ДАНІ1" aria-label="Username" aria-describedby="basic-addon1">
-</div>
-<div class="input-group m-2">
-  <span class="input-group-text" id="basic-addon1">@</span>
-  <input type="text" class="form-control" placeholder="ДАНІ2" aria-label="Username" aria-describedby="basic-addon1">
-</div>
-<div class="input-group m-2">
-  <span class="input-group-text" id="basic-addon1">@</span>
-  <input type="text" class="form-control" placeholder="ДАНІ3" aria-label="Username" aria-describedby="basic-addon1">
-</div>
-
-		<div class="col"><button type="button" class="btn btn-primary mb-2">*Підтвердити*</button></div>
-		<div class="col"><button type="button" class="btn btn-primary mb-2">*Списати*</button></div>
-		<div class="col"><button type="button" class="btn btn-primary mb-2">*Сушити*</button></div>
-		<div class="col"><button type="button" class="btn btn-primary mb-2">*Продати*</button></div>
-	</div>
-
+		<div class="container-lg text-center row g-0">
+			<div class="col"><button type="button" class="btn btn-success mb-2" type="submit" id="approve" name="approve" >*Підтвердити*</button></div>
+			<div class="col"><button type="button" class="btn btn-danger mb-2" type="submit" id="write_off" name="write_off" >*Списати*</button></div>
+			<div class="col"><button type="button" class="btn btn-info mb-2" type="submit" id="dry" name="dry" >*Сушити*</button></div>
+			<div class="col"><button type="button" class="btn btn-warning mb-2" type="submit" id="sell" name="sell" >*Продати*</button></div>
+		</div>
+	</form>
 
 	<table class="table table-striped">
 		<thead>
@@ -62,7 +109,6 @@
 			<?php endforeach; ?>    
 		</tbody>
 	</table>
-</div>
 </div>
 
 <?php include TEMPLATES_PATH."partials".DIRECTORY_SEPARATOR."footer.php";?>
