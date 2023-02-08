46db7e9af23d54b5f2ae828fb821b638764763ff
Oleh <95093144+OlehKrupa@users.noreply.github.com>
2023-02-08 23:58:29 +0200

    Create report.php

---
+++ templates/report.php
@@ -0,0 +1,32 @@
+<?php include TEMPLATES_PATH."partials".DIRECTORY_SEPARATOR."header.php";?>
+<div class="container-lg text-center">
+	<div class="row">
+		<div class="col-10">
+			<div class="container-lg row g-0">
+				<div class="col"><button type="button" class="btn btn-info m-2">*Звіт*</button></div>
+				<div class="col"><button type="button" class="btn btn-info m-2">*Звіт*</button></div>
+				<div class="col"><button type="button" class="btn btn-info m-2">*Звіт*</button></div>
+			</div>
+
+			<div class="container-lg row g-0">
+				<div class="col"><button type="button" class="btn btn-warning m-2">*Зерносховища*</button></div>
+				<div class="col"><button type="button" class="btn btn-warning m-2">*Стандарти*</button></div>
+				<div class="col"><button type="button" class="btn btn-warning m-2">*Склади*</button></div>
+				<div class="col"><button type="button" class="btn btn-warning m-2">*Відправки*</button></div>
+				<div class="col"><button type="button" class="btn btn-warning m-2">*Постачальники*</button></div>
+			</div>
+
+		</div>
+
+		<div class="col-2">
+			<br>Стовп повідомлень типу
+			<br>Зерно 1!
+			<br>Зерно 2 псується!
+			<br>Зерно 3 зіпсувалось!
+			<br>Зерно 4 закінчується!
+			<br>Так, такий же стовп як на індексі
+		</div>
+	</div>
+</div>
+
+<?php include TEMPLATES_PATH."partials".DIRECTORY_SEPARATOR."footer.php";?>
