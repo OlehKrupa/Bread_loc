9a7d03577df12ab1ec66719d79d89ab7957b411d
Oleh <95093144+OlehKrupa@users.noreply.github.com>
2023-02-13 00:57:28 +0200

    Update alert_cell.php
    Переполнение склада добавлено в оповещение

--- alert_cell.php
+++ alert_cell.php
@@ -1,16 +1,26 @@
 <?php 
-$result=$dbConnect->query("select `Crop`.`name` AS `name`,`Crop`.`variety` AS `variety`,`Crop`.`grade` AS `grade`,`Crop`.`moisture` AS `moisture` from `Crop`");
-$list_ = $result->fetchAll(PDO::FETCH_ASSOC);
-
 $alert=[];
 
-foreach ($list_ as $k => $v){
+$result=$dbConnect->query("select `Crop`.`name` AS `name`,`Crop`.`variety` AS `variety`,`Crop`.`grade` AS `grade`,`Crop`.`moisture` AS `moisture` from `Crop`");
+$list_grades = $result->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($list_grades as $k => $v){
 	if ($v['grade']==="Зіпсовано"){
 		array_push($alert, "{$v['name']} {$v['variety']} Зіпсовано!");
 	} elseif (($v['grade']==="Погано") && ($v['moisture']<0.16)) {
 		array_push($alert, "{$v['name']} {$v['variety']} Терміново реалізувати!");
 	} elseif (($v['grade']==="Погано")&& ($v['moisture']>0.16)){
 		array_push($alert, "{$v['name']} {$v['variety']} Просушити або реалізувати!");
+	}
+}
+
+$capacity = $dbConnect->query("SELECT `Crop`.`Warehouse_id`, `Warehouse`.`name` as `name`, sum(`Crop`.`amount`) as `amount`, `Warehouse`.`capacity` as `capacity` FROM `Crop` INNER JOIN `Warehouse` ON `Crop`.`Warehouse_id` = `Warehouse`.`id` GROUP BY `Crop`.`Warehouse_id`");
+$list_capacity = $capacity->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($list_capacity as $key => $value){
+	if ($value['amount']>$value['capacity']){
+		$overload=$value['amount']-$value['capacity'];
+		array_push($alert,"{$value['name']} Переповнено на {$overload} тон!");
 	}
 }
 
