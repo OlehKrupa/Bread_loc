636034ae6bf61084029f2cc54032f48c34592523
Oleh <95093144+OlehKrupa@users.noreply.github.com>
2023-02-12 02:39:34 +0200

    Проверкан на пустые поля

--- templates/crop.php
+++ templates/crop.php
@@ -7,70 +7,89 @@
 		<div class="container-lg text-center row g-0">
 			<div class="col mx-2">
 				<div class="input-group m-2">
-					<span class="input-group-text" id="basic-addon1">Постачальник</span>
-					<select class="form-select" id="supplier_select" name="supplier_select">
+					<span class="input-group-text" id="supplier_select">Постачальник</span>
+					
+					<select class="form-select <?php if(!empty($error['supplier_select'])) echo 'is-invalid' ?>" id="supplier_select" name="supplier_select">
+						<option value="" selected ></option>
 						<?php foreach($supplier as $key=>$value): ?>
-							<option value="<?php echo $value["id"];?>"><?php echo $value["name"];?></option>
+							<option value="<?php echo $value["id"];?>" <?php if ($value["id"]===$supplier_ui){echo "selected";}?> ><?php echo $value["name"];?></option>
 						<?php endforeach; ?>
-						<option selected=""> </option>
 					</select>
 				</div>
+
 				<div class="input-group m-2">
 					<span class="input-group-text" id="warehouse_select">Склад</span>
-					<select class="form-select" id="warehouse_select" name="warehouse_select">
+					<div class="invalid-feedback"><?php echo $error['warehouse_select'] ?? '';?></div>
+					<select class="form-select <?php if(!empty($error['warehouse_select'])) echo 'is-invalid' ?>" id="warehouse_select" name="warehouse_select">
+						<option value="" selected ></option>
 						<?php foreach($warehouse as $key=>$value): ?>
-							<option value="<?php echo $value["id"];?>"><?php echo $value["name"];?></option>
+							<option value="<?php echo $value["id"];?>" <?php if ($value["id"]===$warehouse_ui){echo "selected";}?> ><?php echo $value["name"];?></option>
 						<?php endforeach; ?>
-						<option selected=""> </option>
 					</select>
 				</div>
+
 				<div class="input-group m-2">
 					<span class="input-group-text" id="amount">Кількість тон</span>
-					<input type="text" class="form-control" placeholder="" name="amount" aria-describedby="amount" value="<?php echo $amount_ui ?>">
+					<div class="invalid-feedback"><?php echo $error['amount'] ?? '';?></div>
+					<input type="text" class="form-control <?php if(!empty($error['amount'])) echo 'is-invalid' ?>" placeholder="" name="amount" aria-describedby="amount" value="<?php echo $amount_ui ?>">
 				</div>
+
 				<div class="input-group m-2">
 					<span class="input-group-text" id="name">Культура</span>
-					<input type="text" class="form-control" placeholder="" name="name" aria-describedby="name" value="<?php echo $name_ui ?>">
+					<div class="invalid-feedback"><?php echo $error['name'] ?? '';?></div>
+					<input type="text" class="form-control <?php if(!empty($error['name'])) echo 'is-invalid' ?>" placeholder="" name="name" aria-describedby="name" value="<?php echo $name_ui ?>">
 				</div>
+
 				<div class="input-group m-2">
 					<span class="input-group-text" id="variety">Сорт</span>
-					<input type="text" class="form-control" placeholder="" name="variety" aria-describedby="variety" value="<?php echo $variety_ui ?>">
+					<div class="invalid-feedback"><?php echo $error['variety'] ?? '';?></div>
+					<input type="text" class="form-control <?php if(!empty($error['variety'])) echo 'is-invalid' ?>" placeholder="" name="variety" aria-describedby="variety" value="<?php echo $variety_ui ?>">
 				</div>
+
 			</div>
 			<div class="col mx-2">
 				<div class="input-group m-2">
 					<span class="input-group-text" id="standard_select">Стандарт</span>
-					<select class="form-select" id="standard_select" name="standard_select">
+					<div class="invalid-feedback"><?php echo $error['standard_select'] ?? '';?></div>
+					<select class="form-select <?php if(!empty($error['standard_select'])) echo 'is-invalid' ?>" id="standard_select" name="standard_select">
+						<option value="" selected ></option>
 						<?php foreach($standart as $key=>$value): ?>
-							<option value="<?php echo $value["id"];?>"><?php echo $value["name"];?></option>
+							<option value="<?php echo $value["id"];?>" <?php if ($value["id"]===$standard_ui){echo "selected";}?> ><?php echo $value["name"];?></option>
 						<?php endforeach; ?>
-						<option selected=""> </option>
 					</select>
 				</div>
+
 				<div class="input-group m-2">
 					<span class="input-group-text" id="moisture">Вологість</span>
-					<input type="text" class="form-control" placeholder="" name="moisture" aria-describedby="moisture" value="<?php echo $moisture_ui ?>">
+					<div class="invalid-feedback"><?php echo $error['moisture'] ?? '';?></div>
+					<input type="text" class="form-control <?php if(!empty($error['moisture'])) echo 'is-invalid' ?>" placeholder="" name="moisture" aria-describedby="moisture" value="<?php echo $moisture_ui ?>">
 				</div>
+
 				<div class="input-group m-2">
 					<span class="input-group-text" id="garbage">Сміття</span>
-					<input type="text" class="form-control" placeholder="" name="garbage" aria-describedby="garbage" value="<?php echo $garbage_ui ?>">
+					<div class="invalid-feedback"><?php echo $error['garbage'] ?? '';?></div>
+					<input type="text" class="form-control <?php if(!empty($error['garbage'])) echo 'is-invalid' ?>" placeholder="" name="garbage" aria-describedby="garbage" value="<?php echo $garbage_ui ?>">
 				</div>
+
 				<div class="input-group m-2">
 					<span class="input-group-text" id="minerals">PO₄</span>
-					<input type="text" class="form-control" placeholder="" name="minerals" aria-describedby="minerals" value="<?php echo $minerals_ui ?>">
+					<div class="invalid-feedback"><?php echo $error['minerals'] ?? '';?></div>
+					<input type="text" class="form-control <?php if(!empty($error['minerals'])) echo 'is-invalid' ?>" placeholder="" name="minerals" aria-describedby="minerals" value="<?php echo $minerals_ui ?>">
 				</div>
+
 				<div class="input-group m-2">
 					<span class="input-group-text" id="nature">г/л</span>
-					<input type="text" class="form-control" placeholder="" name="nature" aria-describedby="nature" value="<?php echo $nature_ui ?>">
+					<div class="invalid-feedback"><?php echo $error['nature'] ?? '';?></div>
+					<input type="text" class="form-control <?php if(!empty($error['nature'])) echo 'is-invalid' ?>" placeholder="" name="nature" aria-describedby="nature" value="<?php echo $nature_ui ?>">
 				</div>
+
 			</div>
 		</div>
 
 		<div class="container-lg text-center row g-0">
-			<div class="col"><button class="btn btn-primary mb-2" type="submit" name="add" >*Додати*</button></div>
+			<div class="col"><button class="btn btn-success mb-2" type="submit" name="ok" >ОК</button></div>
 			<div class="col"><button class="btn btn-primary mb-2" type="submit" name="refresh" >Оновити</button></div>
 			<div class="col"><button class="btn btn-secondary mb-2" type="submit" name="clear" >Очистити</button></div>
-			<div class="col"><button class="btn btn-success mb-2" type="submit" name="approve" >*Підтвердити*</button></div>
 			<div class="col"><button class="btn btn-danger mb-2" type="submit" name="write_off" >Списати</button></div>
 			<div class="col"><button class="btn btn-info mb-2" type="submit" name="dry" >Сушити</button></div>
 			<div class="col"><button class="btn btn-warning mb-2" type="submit" name="sell" >Продати</button></div>
