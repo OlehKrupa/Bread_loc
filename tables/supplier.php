cf3e9b535f84860fab60b25e663a1918867264da
Oleh <95093144+OlehKrupa@users.noreply.github.com>
2023-02-13 00:58:24 +0200

    CRUD таблицы поставщики
    С костылем

--- tables/supplier.php
+++ tables/supplier.php
@@ -1,5 +1,100 @@
-<?php
-require_once '../config.php';
+	<?php
+	require_once '../config.php';
+//!!!
+	$chose_id=$_SESSION['chose_id'];
 
-require_once TEMPLATES_PATH."supplier.php";
+	$sort_list = array(
+		'id_asc'=>'`id`',
+		'id_desc'=>'`id` DESC',
+		'name_asc'=>'`name`',
+		'name_desc'=>'`name` DESC',
+		'number_asc'=>'`number`',
+		'number_desc'=>'`number` DESC',
+	);
+	$sort = @$_GET['sort'];
+	if (array_key_exists($sort, $sort_list)){
+		$sort_sql = $sort_list[$sort];
+	} else {
+		$sort_sql=reset($sort_list);
+	}
+
+	$result = $dbConnect->query("select * from Supplier order by {$sort_sql}");
+	$list = $result->fetchAll(PDO::FETCH_ASSOC);
+
+	$fields=["name","number"];
+
+	if (isset($_POST['ok'])){
+		if (!empty($_POST)){
+			$name_ui=htmlspecialchars($_POST['name']);
+			$number_ui=htmlspecialchars($_POST['number']);
+			$error=[];
+			foreach ($_POST as $k => $v) {
+				if (in_array($k, $fields) && empty($v)){
+					$error[$k]="field must be filled!";
+				}
+			}
+			//проверка на непустые поля
+			if (empty($error)){
+				if (empty($chose_id)){
+					$stmt = $dbConnect->prepare("INSERT INTO `Supplier` (
+						`name`,
+						`number`
+						) 
+					VALUES 
+					( 
+						:n,  
+						:nu
+					)"
+				);
+					$stmt->execute(["n"=>$name_ui,"nu"=>$number_ui]);
+				}else{
+					$stmt = $dbConnect->prepare("UPDATE `Supplier`
+						SET
+						`name`=:n,
+						`number`=:nu
+						where `id`=:id");
+					$stmt->execute(["n"=>$name_ui,"nu"=>$number_ui,"id"=>$chose_id]);
+
+				}
+			}
+		}
+		require_once TEMPLATES_PATH."supplier.php";
+	}
+
+	if (empty($chose_id)){
+		//add
+	}else{
+		//update
+		foreach ($list as $key => $value) {
+			if ($value['id']===$chose_id){
+				$name_ui=$value['name'];
+				$number_ui=$value['number'];
+			}
+		}
+	}
+
+	if (isset($_POST['clear'])){
+	//Сделать реальную очистку chose_id
+	//$chose_id="";
+		$name_ui="";
+		$number_ui="";
+	}
+
+	if (isset($_POST['delete'])){
+		$delete = $dbConnect->prepare("DELETE from Supplier where id = :id");
+		$delete->execute(["id"=>$chose_id]);
+	}
+
+	function sort_link_th($title, $a, $b) {
+		$sort = @$_GET['sort'];
+		if ($sort == $a) {
+			return '<a class="active" href="?sort=' . $b . '">' . $title . ' <i></i></a>';
+		} elseif ($sort == $b) {
+			return '<a class="active" href="?sort=' . $a . '">' . $title . ' <i></i></a>';  
+		} else {
+			return '<a href="?sort=' . $a . '">' . $title . '</a>';  
+		}
+	}
+
+	require_once TEMPLATES_PATH."supplier.php";
 ?>
--- templates/supplier.php
+++ templates/supplier.php
@@ -1,6 +1,52 @@
 <?php include TEMPLATES_PATH."partials".DIRECTORY_SEPARATOR."header.php";?>
+
 <div class="container-lg">
-	<h1>Постачальники</h1>
+	<form action="/tables/supplier.php" method="post">
+		
+		<div class="container-lg text-center row g-0">
+			<div class="col mx-2">
+				<div class="input-group m-2">
+					<span class="input-group-text" id="name">Назва</span>
+					<div class="invalid-feedback"><?php echo $error['name'] ?? '';?></div>
+					<textarea class="form-control <?php if(!empty($error['name'])) echo 'is-invalid' ?>" placeholder="" id="name" name="name" aria-describedby="name"><?php if (!empty($name_ui)){echo $name_ui;} ?></textarea>
+				</div>
+			</div>
+			<div class="col mx-2">
+				<div class="input-group m-2">
+					<span class="input-group-text" id="number">Номер телефону</span>
+					<div class="invalid-feedback"><?php echo $error['number'] ?? '';?></div>
+					<input type="text" class="form-control <?php if(!empty($error['number'])) echo 'is-invalid' ?>" placeholder="" name="number" aria-describedby="number" value="<?php if (!empty($number_ui)){echo $number_ui;} ?>">
+				</div>
+			</div>
+		</div>
+
+		<div class="container-lg text-center row g-0">
+			<div class="col"><button class="btn btn-success mb-2" type="submit" name="ok" >ОК</button></div>
+			<div class="col"><button class="btn btn-secondary mb-2" type="submit" name="clear" >Очистити</button></div>
+			<div class="col"><button class="btn btn-danger mb-2" type="submit" name="delete" >Видалити</button></div>
+		</div>
+
+	</form>
+
+	<table class="table">
+		<script src="../JS/index.js"></script>
+		<thead>
+			<tr class="table-info">
+				<th><?php echo sort_link_th('Айді','id_asc','id_desc'); ?></th>
+				<th><?php echo sort_link_th('Назва','name_asc','name_desc'); ?></th>
+				<th><?php echo sort_link_th('Номер телефону','number_asc','number_desc'); ?></th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php foreach ($list as $row): ?>
+				<tr class="table-primary" onclick="get_element_by_click(<?php echo $row['id']; ?>)" > 
+					<td><?php echo $row['id']; ?></td>
+					<td><?php echo $row['name']; ?></td>
+					<td><?php echo $row['number']; ?></td>
+				</tr >
+			<?php endforeach; ?> 
+		</tbody>
+	</table>
 </div>
 
 <?php include TEMPLATES_PATH."partials".DIRECTORY_SEPARATOR."footer.php";?>
