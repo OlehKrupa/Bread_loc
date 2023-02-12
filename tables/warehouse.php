e0a407a237b1007eaf316518fa0c7b82f19593ae
Oleh <95093144+OlehKrupa@users.noreply.github.com>
2023-02-13 00:58:40 +0200

    CRUD таблицы склады
    с костылем

--- tables/warehouse.php
+++ tables/warehouse.php
@@ -1,5 +1,104 @@
-<?php
-require_once '../config.php';
+	<?php
+	require_once '../config.php';
+//!!!
+	$chose_id=$_SESSION['chose_id'];
 
-require_once TEMPLATES_PATH."warehouse.php";
+	$sort_list = array(
+		'id_asc'=>'`id`',
+		'id_desc'=>'`id` DESC',
+		'name_asc'=>'`name`',
+		'name_desc'=>'`name` DESC',
+		'address_asc'=>'`address`',
+		'address_desc'=>'`address` DESC',
+	);
+	$sort = @$_GET['sort'];
+	if (array_key_exists($sort, $sort_list)){
+		$sort_sql = $sort_list[$sort];
+	} else {
+		$sort_sql=reset($sort_list);
+	}
+
+	$result = $dbConnect->query("select * from Warehouse order by {$sort_sql}");
+	$list = $result->fetchAll(PDO::FETCH_ASSOC);
+
+	$fields=["name","address","capacity"];
+
+	if (isset($_POST['ok'])){
+		if (!empty($_POST)){
+			$name_ui=htmlspecialchars($_POST['name']);
+			$address_ui=htmlspecialchars($_POST['address']);
+			$capacity_ui=htmlspecialchars($_POST['capacity']);
+			$error=[];
+			foreach ($_POST as $k => $v) {
+				if (in_array($k, $fields) && empty($v)){
+					$error[$k]="field must be filled!";
+				}
+			}
+			//проверка на непустые поля
+			if (empty($error)){
+				if (empty($chose_id)){
+					$stmt = $dbConnect->prepare("INSERT INTO `Warehouse` (
+						`name`,
+						`address`,
+						`capacity`
+						) 
+					VALUES 
+					( 
+						:n,  
+						:a,
+						:c
+					)"
+				);
+					$stmt->execute(["n"=>$name_ui,"a"=>$address_ui,"c"=>$capacity_ui]);
+				}else{
+					$stmt = $dbConnect->prepare("UPDATE `Warehouse`
+						SET
+						`name`=:n,
+						`address`=:a,
+						`capacity`=:c
+						where `id`=:id");
+					$stmt->execute(["n"=>$name_ui,"a"=>$address_ui,"c"=>$capacity_ui,"id"=>$chose_id]);
+				}
+			}
+		}
+		require_once TEMPLATES_PATH."warehouse.php";
+	}
+
+	if (empty($chose_id)){
+		//add
+	}else{
+		//update
+		foreach ($list as $key => $value) {
+			if ($value['id']===$chose_id){
+				$name_ui=$value['name'];
+				$address_ui=$value['address'];
+				$capacity_ui=$value['capacity'];
+			}
+		}
+	}
+
+	if (isset($_POST['clear'])){
+	//Сделать реальную очистку chose_id
+	//$chose_id="";
+		$name_ui="";
+		$address_ui="";
+		$capacity_ui="";
+	}
+
+	if (isset($_POST['delete'])){
+		$delete = $dbConnect->prepare("DELETE from Warehouse where id = :id");
+		$delete->execute(["id"=>$chose_id]);
+	}
+
+	function sort_link_th($title, $a, $b) {
+		$sort = @$_GET['sort'];
+		if ($sort == $a) {
+			return '<a class="active" href="?sort=' . $b . '">' . $title . ' <i></i></a>';
+		} elseif ($sort == $b) {
+			return '<a class="active" href="?sort=' . $a . '">' . $title . ' <i></i></a>';  
+		} else {
+			return '<a href="?sort=' . $a . '">' . $title . '</a>';  
+		}
+	}
+	require_once TEMPLATES_PATH."warehouse.php";
 ?>
--- templates/warehouse.php
+++ templates/warehouse.php
@@ -1,6 +1,60 @@
 <?php include TEMPLATES_PATH."partials".DIRECTORY_SEPARATOR."header.php";?>
 <div class="container-lg">
-	<h1>Склади</h1>
+	<form action="/tables/warehouse.php" method="post">
+		
+		<div class="container-lg text-center row g-0">
+			<div class="col mx-2">
+				<div class="input-group m-2">
+					<span class="input-group-text" id="name">Назва</span>
+					<div class="invalid-feedback"><?php echo $error['name'] ?? '';?></div>
+					<textarea class="form-control <?php if(!empty($error['name'])) echo 'is-invalid' ?>" id="name" name="name" aria-describedby="name"><?php if (!empty($name_ui)){echo $name_ui;} ?></textarea>
+				</div>
+			</div>
+			<div class="col mx-2">
+				<div class="input-group m-2">
+					<span class="input-group-text" id="address">Адреса</span>
+					<div class="invalid-feedback"><?php echo $error['address'] ?? '';?></div>
+					<textarea class="form-control <?php if(!empty($error['address'])) echo 'is-invalid' ?>" id="address" name="address" aria-describedby="address" ><?php if (!empty($address_ui)){echo $address_ui;} ?></textarea>
+				</div>
+			</div>
+			<div class="col mx-2">
+				<div class="input-group m-2">
+					<span class="input-group-text" id="capacity">Ємність</span>
+					<div class="invalid-feedback"><?php echo $error['capacity'] ?? '';?></div>
+					<input type="text" class="form-control <?php if(!empty($error['capacity'])) echo 'is-invalid' ?>" placeholder="" name="capacity" aria-describedby="capacity" value="<?php echo $capacity_ui ?>">
+				</div>
+			</div>
+		</div>
+
+		<div class="container-lg text-center row g-0">
+			<div class="col"><button class="btn btn-success mb-2" type="submit" name="ok" >ОК</button></div>
+			<div class="col"><button class="btn btn-secondary mb-2" type="submit" name="clear" >Очистити</button></div>
+			<div class="col"><button class="btn btn-danger mb-2" type="submit" name="delete" >Видалити</button></div>
+		</div>
+
+	</form>
+
+	<table class="table">
+		<script src="../JS/index.js"></script>
+		<thead>
+			<tr class="table-info">
+				<th><?php echo sort_link_th('Айді','id_asc','id_desc'); ?></th>
+				<th><?php echo sort_link_th('Назва','name_asc','name_desc'); ?></th>
+				<th><?php echo sort_link_th('Адреса','address_asc','address_desc'); ?></th>
+				<th><?php echo sort_link_th('Ємність','capacity_asc','capacity_desc'); ?></th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php foreach ($list as $row): ?>
+				<tr class="table-primary" onclick="get_element_by_click(<?php echo $row['id']; ?>)" > 
+					<td><?php echo $row['id']; ?></td>
+					<td><?php echo $row['name']; ?></td>
+					<td><?php echo $row['address']; ?></td>
+					<td><?php echo $row['capacity']; ?></td>
+				</tr >
+			<?php endforeach; ?> 
+		</tbody>
+	</table>
 </div>
 
 <?php include TEMPLATES_PATH."partials".DIRECTORY_SEPARATOR."footer.php";?>
