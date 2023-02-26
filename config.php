3188793b9d191a5c48162bd8f561ae6a8007b75c
Oleh <95093144+OlehKrupa@users.noreply.github.com>
2023-02-26 21:16:12 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,6 @@
 <?php
 session_start();
 date_default_timezone_set('UTC');
-
-//!!!
-$_SESSION['chose_id']=0;
-$_SESSION['sell_id']=0;
  
 define("ROOT_PATH", dirname(__FILE__));
 define("TEMPLATES_PATH", dirname(__FILE__).DIRECTORY_SEPARATOR."templates".DIRECTORY_SEPARATOR);
