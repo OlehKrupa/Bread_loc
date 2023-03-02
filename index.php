632f302cb85879e4fd7688cf59a8ffc84ef7304a
Oleh <95093144+OlehKrupa@users.noreply.github.com>
2023-03-02 03:25:07 +0200

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ if (isset($_POST['index_chose_id'])){
 }
 
 if (!empty($_SESSION['index_chose_id'])){
-$chose_id=$_SESSION['index_chose_id'];
+	$chose_id=$_SESSION['index_chose_id'];
 }
 
 if (empty($_SESSION['user'])){
@@ -15,21 +15,37 @@ if (empty($_SESSION['user'])){
 }
 
 if (isset($_POST['write_off'])){
-	$delete = $dbConnect->prepare("DELETE from Crop where id = :id");
-	$delete->execute(["id"=>$chose_id]);
+	if(!empty($chose_id)){
+		$delete = $dbConnect->prepare("DELETE from Crop where id = :id");
+		$delete->execute(["id"=>$chose_id]);
+		header("Refresh:0");
+	} else{
+		echo "<script type='text/javascript'>alert('Помилка! Зерно на списання не обране!');</script>";
+		header("Refresh:0");
+	}
 }
 
 if (isset($_POST['dry'])){
-	$_SESSION['dry_id']=$chose_id;
-	require_once 'dry.php';
-	require_once 'grade.php';
-	header("Refresh:0");
+	if(!empty($chose_id)){
+		$_SESSION['dry_id']=$chose_id;
+		require_once 'dry.php';
+		require_once 'grade.php';
+		header("Refresh:0");
+	} else {
+		echo "<script type='text/javascript'>alert('Помилка! Зерно на сушку не обране!');</script>";
+		header("Refresh:0");
+	}
 }
 
 if (isset($_POST['sell'])){
-	$_SESSION['sell_id']=$chose_id;
-	header("location: /tables/consignment.php");
-	die();
+	if(!empty($chose_id)){
+		$_SESSION['sell_id']=$chose_id;
+		header("location: /tables/consignment.php");
+		die();
+	} else {
+		echo "<script type='text/javascript'>alert('Помилка! Зерно на продаж не обрано!');</script>";
+		header("Refresh:0");
+	}
 }
 
 $result = $dbConnect->query("select `Crop`.`id` AS `id`,`Supplier`.`name` AS `supplier_name`,`Crop`.`date` AS `date`,`Warehouse`.`name` AS `warehouse_name`,`Crop`.`amount` AS `amount`,`Standard`.`name` AS `standard_name`,`Crop`.`name` AS `name`,`Crop`.`variety` AS `variety`,`Crop`.`grade` AS `grade`,`Crop`.`moisture` AS `moisture`,`Crop`.`garbage` AS `garbage`,`Crop`.`minerals` AS `minerals`,`Crop`.`nature` AS `nature` from (((`Crop` join `Warehouse` on((`Crop`.`Warehouse_id` = `Warehouse`.`id`))) join `Supplier` on((`Crop`.`Supplier_id` = `Supplier`.`id`))) join `Standard` on((`Crop`.`Standard_id` = `Standard`.`id`))) where `amount` > 0");
