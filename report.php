3eec596b5d0d04b4336d7829d90d19fcec9f815d
Oleh <95093144+OlehKrupa@users.noreply.github.com>
2023-03-08 01:06:32 +0200

    не робіт

--- report.php
+++ report.php
@@ -1,7 +1,7 @@
 <?php 
 require_once "config.php";
 
-require_once 'vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
