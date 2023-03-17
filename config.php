8bd8b69c6663fc4fdf2a92dc63feb8347cd0b186
Oleh <95093144+OlehKrupa@users.noreply.github.com>
2023-03-17 15:43:06 +0200

    Update config.php

--- config.php
+++ config.php
@@ -5,6 +5,7 @@ date_default_timezone_set('Europe/Kyiv');
 define("ROOT_PATH", dirname(__FILE__));
 define("TEMPLATES_PATH", dirname(__FILE__).DIRECTORY_SEPARATOR."templates".DIRECTORY_SEPARATOR);
 define("TABLES_PATH", dirname(__FILE__).DIRECTORY_SEPARATOR."tables".DIRECTORY_SEPARATOR);
+define("CLASS_PATH", dirname(__FILE__).DIRECTORY_SEPARATOR."classes".DIRECTORY_SEPARATOR);
 
 define("DB_NAME","bread.loc");
 define("DB_USER","grain_administrator");
